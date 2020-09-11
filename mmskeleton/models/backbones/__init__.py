e795326c745854d360f52369e2f143d30af808e7
niu <njydyx@icloud.com>
2020-09-11 10:57:26 +0800

    Add ALN10

--- mmskeleton/models/backbones/__init__.py
+++ mmskeleton/models/backbones/__init__.py
@@ -11,4 +11,5 @@ from .ALN6 import ST_GCN_ALN6
 from .ALN7 import ST_GCN_ALN7
 from .ALN8 import ST_GCN_ALN8
 from .ALN9 import ST_GCN_ALN9
+from .ALN10 import ST_GCN_ALN10
 from .hrnet import HRNet
