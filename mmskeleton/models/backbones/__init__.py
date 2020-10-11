29afe6fa226438b4bfbd0cb268d571c3b87b0510
Your Name <njydyx@icloud.com>
2020-10-11 19:19:21 +0800

    add ALN59

--- mmskeleton/models/backbones/__init__.py
+++ mmskeleton/models/backbones/__init__.py
@@ -60,4 +60,5 @@ from .ALN55 import ST_GCN_ALN55
 from .ALN56 import ST_GCN_ALN56
 from .ALN57 import ST_GCN_ALN57
 from .ALN58 import ST_GCN_ALN58
+from .ALN59 import ST_GCN_ALN59
 from .hrnet import HRNet
