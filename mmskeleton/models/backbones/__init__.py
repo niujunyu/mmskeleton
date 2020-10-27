70501942a4963acebcf8414a42fd62e2bb1a8d55
Your Name <njydyx@icloud.com>
2020-10-27 15:05:26 +0800

    aln 72

--- mmskeleton/models/backbones/__init__.py
+++ mmskeleton/models/backbones/__init__.py
@@ -74,5 +74,9 @@ from .ALN69 import ST_GCN_ALN69
 from .ALN70 import ST_GCN_ALN70
 from .ALN71 import ST_GCN_ALN71
 from .ALN72 import ST_GCN_ALN72
+
+from .ALN73 import ST_GCN_ALN73
+
+from .ALN73 import ST_GCN_ALN73
 from .TSA1 import ST_GCN_TSA1
 from .hrnet import HRNet
